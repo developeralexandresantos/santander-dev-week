a583a05a78165cf974de2837853a52c06afc1f94
Alexandre Aparecido dos Santos <68973579+developeralexandresantos@users.noreply.github.com>
2025-03-30 02:41:13 -0300

    Implementação da camada de domínio

---
+++ src/main/java/br/com/developeralexandresantos/santander_dev_week/domain/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package br.com.developeralexandresantos.santander_dev_week.domain.repository;
+
+import br.com.developeralexandresantos.santander_dev_week.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
